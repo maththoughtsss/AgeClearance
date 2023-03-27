32dd16c173c73eb12b03995bbc18997663aa537b
maththoughtsss <jonathan.munoz01@mayor.cl>
2023-03-27 01:11:03 -0300

    if statement exercise

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AgeClearance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string? strAge;
+
+            Console.WriteLine("Enter your age:");
+            strAge = Console.ReadLine();
+
+            int.TryParse(strAge,out age);
+
+            if(age >= 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("You're an adult. Welcome!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You're too young. Denied Access.");
+            }
+        }
+    }
+}
